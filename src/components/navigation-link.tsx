7fd6dde9aea054982b03687ffbc7b5eaaeb2b67b
Kuba <jakub.pusiak@gmail.com>
2025-05-10 11:55:31 +0200

    📗 Navigation, pages, components

---
+++ src/components/navigation-link.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+
+interface Props {
+  href: string;
+  children: React.ReactNode;
+  isActive: boolean;
+}
+
+export const NavigationLink = ({ href, children, isActive }: Props) => {
+  return (
+    <a
+      href={href}
+      className={cn(
+        'flex items-center text-sm font-medium',
+        isActive ? "text-primary" : "text-muted-foreground"
+      )}
+      aria-current={isActive ? "page" : undefined}
+    >
+      <span className={cn(
+        'w-2 h-2 rounded-full bg-brand-1 mr-2',
+        isActive ? "opacity-100" : "opacity-0"
+      )} />
+      {children}
+    </a>
+  );
+}
